c00e8d620f644bf15527dcec075f2cfc8cd6f56a
LsaBarbosa <lsa94.contato@gmail.com>
2025-09-20 15:41:37 -0300

    update checkin

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL;
+//export const API_BASE_URL = "https://kronos-tech-solutions-kts.onrender.com/";
+export const API_BASE_URL = "http://localhost:8080/";
